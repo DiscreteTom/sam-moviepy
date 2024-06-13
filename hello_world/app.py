f763dbc5b467802a1dc07782dbab0ddfa740f5f9
DiscreteTom <discrete_tom@outlook.com>
2024-06-13 05:45:49 +0000

    pass local test

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,29 @@
 import json
+from moviepy.editor import *
 
-# import requests
+
+def main():
+    video = VideoFileClip("buggy.mp4")
+
+    # Make the text. Many more options are available.
+    txt_clip = TextClip("My Holidays 2013", fontsize=70, color="white").set_duration(2)
+
+    result = CompositeVideoClip([video, txt_clip])  # Overlay text on video
+    result.write_videofile("edited.mp4", fps=25)  # Many options...
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    main()
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps(
+            {
+                "message": "hello world",
+            }
+        ),
     }
+
+
+if __name__ == "__main__":
+    main()
